21f0954b51418ad3a4629bf3f88d5e0c7156b75c
itqonibras <itqonnibras@gmail.com>
2025-01-04 10:56:51 +0700

    feat: create default router

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -10,4 +10,5 @@ router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('', include(router.urls))
 ]
